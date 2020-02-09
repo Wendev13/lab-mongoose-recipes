fa1a748b48f23d6aff1d577d9c6fccaaddf81ef9
Wendy Evora <wendy.evora@gmail.com>
2020-02-09 22:57:59 +0100

    done

--- index.js
+++ index.js
@@ -11,3 +11,25 @@ mongoose
   })
   .then(x => console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`))
   .catch(err => console.error('Error connecting to mongo', err));
+
+
+  const newRecipe = { title: 'Chocolate cake', level: 'Easy Peasy', ingredients: [ 'Flour', 'Sugar', 'Eggs', 'Chocolate', 'Butter' ], cuisine: true, dishType: 'Dessert', image: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.davidlebovitz.com%2Ffrench-chocolate-cake-recipe%2F&psig=AOvVaw2pYvO8FjaDEcg_zwHQf2MR&ust=1580400854020000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCMj46pGaqecCFQAAAAAdAAAAABAE', duration: 30, creator: 'Wendy' };
+
+
+  Recipe.create(newRecipe, (error, user) => {
+  if (error) {
+    console.log('An error happened:', error);
+    return;
+  }
+  console.log('The new recipe has been successfully added', user);
+});
+
+  Recipe.findByIdAndUpdate('5e31b0fe76c87b65aa65d132', { duration: 100 } )
+  .then(console.log("Recipe updated"))
+  .catch(console.log("Recipe has not been updated"));
+
+  Recipe.deleteOne({ title: 'Carrot Cake' })
+  .then(console.log("Recipe removed"))
+  .catch(console.log("couldn't removed the recipe"));
+
+  //mongoose.connection.close()
